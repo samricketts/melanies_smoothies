f80ac94a6824d45a6c8c17dc9b5458e9c0db4619
samricketts <131821979+samricketts@users.noreply.github.com>
2024-11-19 14:16:11 -0600

    Update streamlit_app.py
    iloc function

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,10 @@ if ingredients_list:
 
     for fruits in ingredients_list:
         ingredients_string += fruits + ' '
+
+        search_on  = pd_df.loc[pd_df['FRUIT_NAME'] == fruits, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruits,' is ', search_on, '.')
+        
         st.subheader(fruits + ' Nutrition Information')
         smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruits)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)        
