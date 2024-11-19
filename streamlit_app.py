75c19f73269bf5ac6c263199e1f7bad5f68cb5ea
samricketts <131821979+samricketts@users.noreply.github.com>
2024-11-19 11:51:02 -0600

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list:
 
 #new section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_resonse=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+smoothiefroot_response=requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
 
 
